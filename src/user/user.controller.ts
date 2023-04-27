c762ac2a23c9e311eda14b8edc5764859c4a4eae
CarlosLNeto <carloslavorneto@gmail.com>
2023-04-27 18:08:18 -0400

    add middleware and ParamId decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,6 +4,7 @@ import { UpdatePutUserDTO } from './dto/update-put-user.dto';
 import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
 import { UserService } from './user.service';
 import { LogInterceptor } from 'src/interceptors/log.interceptors';
+import { ParamId } from 'src/decorators/param-id.decorator';
 
 @UseInterceptors(LogInterceptor)
 @Controller('users')
@@ -23,22 +24,22 @@ export class UserController {
   }
 
   @Get(':id')
-  async show(@Param('id', ParseIntPipe) id: number){
+  async show(@ParamId() id: number){
     return this.userService.show(id);
   }
 
   @Put(':id')
-  async update(@Body() data: UpdatePutUserDTO, @Param('id', ParseIntPipe) id: number){
+  async update(@Body() data: UpdatePutUserDTO, @ParamId() id: number){
     return this.userService.update(id, data);
   }
 
   @Patch(':id')
-  async updatePartial(@Body() data: UpdatePatchUserDTO, @Param('id', ParseIntPipe) id: number){
+  async updatePartial(@Body() data: UpdatePatchUserDTO, @ParamId() id: number){
     return this.userService.updatePartial(id, data);
   }
 
   @Delete(':id')
-  async delete(@Param('id', ParseIntPipe) id: number){
+  async delete(@ParamId() id: number){
     return this.userService.delete(id);
   }
 }
