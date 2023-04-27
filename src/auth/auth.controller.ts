3582927cd6fca13ddd3d886d9b34119430a965c0
CarlosLNeto <carloslavorneto@gmail.com>
2023-04-27 19:14:07 -0400

    update register route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,10 +3,13 @@ import { AuthLoginDTO } from "./dto/auth-login.dto";
 import { AuthRegisterDTO } from "./dto/auth-register.dto";
 import { AuthForgetDTO } from "./dto/auth-forget.dto";
 import { AuthResetDTO } from "./dto/auth-reset.dto";
+import { UserService } from "src/user/user.service";
 
 
 @Controller('auth')
 export class AuthController {
+
+    constructor(private readonly userService: UserService){}
 
     @Post('login')
     async login(@Body() body: AuthLoginDTO){
@@ -15,7 +18,7 @@ export class AuthController {
 
     @Post('register')
     async register(@Body() body: AuthRegisterDTO){
-
+        return this.userService.create(body);
     }
 
     @Post('forget')
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,17 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { AuthController } from "./auth.controller";
+import { UserModule } from "src/user/user.module";
 
 
 @Module({
-    imports: [JwtModule.register({
+    imports: [
+        JwtModule.register({
         secret: "tPy,;h^/V5=-nvYxE2<Mp]`CwT*bed8k"
-    })]
+        }),
+        UserModule
+    ],
+    controllers: [AuthController]
 
 })
 export class AuthModule{
