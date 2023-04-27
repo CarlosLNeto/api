2c3a77696a9163d183a9d4747e68d350ea08fe4a
CarlosLNeto <carloslavorneto@gmail.com>
2023-04-27 17:29:42 -0400

    fix Interceptor to all routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,12 +5,13 @@ import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
 import { UserService } from './user.service';
 import { LogInterceptor } from 'src/interceptors/log.interceptors';
 
+@UseInterceptors(LogInterceptor)
 @Controller('users')
 export class UserController {
 
   constructor(private readonly userService: UserService){}
 
-  @UseInterceptors(LogInterceptor)
+  
   @Post()
   async create(@Body() data: CreateUserDTO) {
     return this.userService.create(data);
