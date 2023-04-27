04aea6699ea6c54a393f896aaaba567eaa10fec8
CarlosLNeto <carloslavorneto@gmail.com>
2023-04-26 23:00:36 -0400

    add gets to controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
@@ -6,5 +6,15 @@ export class UserController {
   @Post()
   async create(@Body() body) {
     return {body};
+  }
+
+  @Get()
+  async read(){
+    return {user:[]};
+  }
+
+  @Get(':id')
+  async readOne(@Param() param){
+    return {users:{ }, param}
   }
 }
