dfd99bd7f4432e915f1b5884581a467115136ba5
CarlosLNeto <carloslavorneto@gmail.com>
2023-04-27 19:06:57 -0400

    add Auth Dtos and Routes login and register

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { AuthLoginDTO } from "./dto/auth-login.dto";
+import { AuthRegisterDTO } from "./dto/auth-register.dto";
+
+
+@Controller('auth')
+export class AuthController {
+
+    @Post('login')
+    async login(@Body() body: AuthLoginDTO){
+
+    }
+
+    @Post('register')
+    async register(@Body() body: AuthRegisterDTO){
+        
+    }
+
+}
