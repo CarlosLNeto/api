4dce332ae141c259210a8757d929c43cd0d425a7
CarlosLNeto <carloslavorneto@gmail.com>
2023-04-27 16:55:40 -0400

    add put and patch to use database

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { CreateUserDTO } from "./dto/create-user.dto";
 import { PrismaService } from "src/prisma/prisma.service";
+import { UpdatePatchUserDTO } from "./dto/update-patch-user.dto";
+import { UpdatePutUserDTO } from "./dto/update-put-user.dto";
 
 @Injectable()
 export class UserService {
@@ -29,4 +31,39 @@ export class UserService {
         });
     }
 
+
+    async update(id: number, {email, name, password, birthAt}: UpdatePutUserDTO){
+
+        return this.prisma.user.update({
+            data: {email, name, password, birthAt: birthAt ? new Date(birthAt) : null},
+            where:{
+                id
+            }
+        })
+    }
+
+    async updatePartial(id: number, {email, name, password, birthAt}: UpdatePatchUserDTO){
+
+        const data: any = {};
+
+        if(birthAt){
+            data.birthAt = new Date(birthAt);
+        }
+        if(email){
+            data.email = email;
+        }
+        if(name){
+            data.name = name;
+        }
+        if(password){
+            data.password = password;
+        }
+
+        return this.prisma.user.update({
+            data,
+            where:{
+                id
+            }
+        })
+    }
 }
