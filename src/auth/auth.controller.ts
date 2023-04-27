b927341735e3b2f2c9ba387f155b4a7a9715ab21
CarlosLNeto <carloslavorneto@gmail.com>
2023-04-27 19:27:35 -0400

    fix auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,16 +4,19 @@ import { AuthRegisterDTO } from "./dto/auth-register.dto";
 import { AuthForgetDTO } from "./dto/auth-forget.dto";
 import { AuthResetDTO } from "./dto/auth-reset.dto";
 import { UserService } from "src/user/user.service";
+import { AuthService } from "./auth.service";
 
 
 @Controller('auth')
 export class AuthController {
 
-    constructor(private readonly userService: UserService){}
+    constructor(private readonly userService: UserService,
+                private readonly authService: AuthService
+        ){}
 
     @Post('login')
-    async login(@Body() body: AuthLoginDTO){
-
+    async login(@Body() {email, password}: AuthLoginDTO){
+        return this.authService.login(email, password);
     }
 
     @Post('register')
@@ -22,13 +25,13 @@ export class AuthController {
     }
 
     @Post('forget')
-    async forget(@Body() body: AuthForgetDTO){
-        
+    async forget(@Body() {email}: AuthForgetDTO){
+        return this.authService.forget(email);
     }
 
     @Post('reset')
-    async reset(@Body() body: AuthResetDTO){
-        
+    async reset(@Body() {password, token}: AuthResetDTO){
+        return this.authService.reset(password, token);
     }
 
 }
