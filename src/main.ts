99f44e09d6404c2eb3dfb87672a9b37cff2409fc
CarlosLNeto <carloslavorneto@gmail.com>
2023-04-27 23:22:59 -0400

    commit test

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { LogInterceptor } from './interceptors/log.interceptors';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   //app.useGlobalInterceptors(new LogInterceptor());
   await app.listen(3000);
+
 }
 bootstrap();
